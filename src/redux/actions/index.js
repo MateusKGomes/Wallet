d56b6dd6b2fa7c2ec2d603f58a75e7f36ee09a71
Mateus Kulik <mateuskulik30@gmail.com>
2023-02-02 10:26:50 -0300

    Adiciona requisito 3, formulário para preencher sobre a despesa e requisição para a API

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,39 @@
+import coinApi from '../../services/API';
+
 export const ADD_EMAIL = 'ADD_EMAIL';
+export const API_SUCESS = 'API_SUCESS';
+export const API_ERROR = 'API_ERROR';
+export const REQUEST_API = 'REQUEST_API';
 
 export const addEmail = (state) => ({
   type: ADD_EMAIL,
   payload: state,
 });
+
+const requestApiStarted = () => ({
+  type: REQUEST_API,
+});
+
+const receiveSucess = (payload) => ({
+  type: API_SUCESS,
+  payload,
+});
+
+const receiveError = (error) => ({
+  type: API_ERROR,
+  payload: error,
+});
+
+const coinRequest = () => async (dispatch) => {
+  try {
+    dispatch(requestApiStarted());
+    const response = await coinApi();
+    const coins = Object.keys(response);
+    const remove = coins.filter((coin) => coin !== 'USDT');
+    dispatch(receiveSucess(remove));
+  } catch (error) {
+    dispatch(receiveError(error));
+  }
+};
+
+export default coinRequest;
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,30 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { API_SUCESS, REQUEST_API, API_ERROR } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+};
+
+const coinRedcuer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case API_SUCESS:
+    return {
+      ...state,
+      currencies: action.payload,
+    };
+  case REQUEST_API:
+    return {
+      ...state,
+    };
+  case API_ERROR:
+    return {
+      ...state,
+      errorMessage: action.payload,
+
+    };
+  default:
+    return state;
+  }
+};
+
+export default coinRedcuer;
